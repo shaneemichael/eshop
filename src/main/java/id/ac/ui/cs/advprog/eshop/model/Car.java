47606fe69b9ba82ff09ae77a77d9b858d8c0c1aa
shaneemichael <shanemichaeltanata@gmail.com>
2025-02-26 09:11:05 +0700

    Add Features: Implement constructors and update method in Car class (Open-closed principle)

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -8,4 +8,25 @@ public class Car {
     private String carName;
     private String carColor;
     private int carQuantity;
+
+    public Car() {
+        this.carId = null;
+        this.carName = null;
+        this.carColor = null;
+        this.carQuantity = 0;
+    }
+
+    public Car(String carId, String carName, String carColor, int carQuantity) {
+        this.carId = carId;
+        this.carName = carName;
+        this.carColor = carColor;
+        this.carQuantity = carQuantity;
+    }
+
+    public Car update(Car updateCar) {
+        this.setCarName(updateCar.getCarName());
+        this.setCarColor(updateCar.getCarColor());
+        this.setCarQuantity(updateCar.getCarQuantity());
+        return this;
+    }
 }
